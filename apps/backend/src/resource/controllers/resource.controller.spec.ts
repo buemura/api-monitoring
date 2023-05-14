ec83880d411274327e57446b7195d2752f1632ad
buemura <bruno.uemura1@gmail.com>
2023-05-14 01:25:58 -0300

    test: adds resource service and controller test

--- apps/backend/src/resource/controllers/resource.controller.spec.ts
+++ apps/backend/src/resource/controllers/resource.controller.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResourceController } from './resource.controller';
+import { ResourceService } from '../services/resource.service';
 
 describe('ResourceController', () => {
   let controller: ResourceController;
+  let resourceServiceMock: Partial<ResourceService>;
 
   beforeEach(async () => {
+    resourceServiceMock = {
+      getResources: jest.fn().mockReturnValue([]),
+      getById: jest.fn().mockReturnValue({}),
+      getByName: jest.fn().mockReturnValue([]),
+      createResource: jest.fn().mockReturnValue({}),
+      updateResource: jest.fn().mockReturnValue({}),
+      remove: jest.fn().mockReturnValue({}),
+      checkResource: jest.fn().mockReturnValue({}),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ResourceController],
+      providers: [
+        {
+          provide: ResourceService,
+          useValue: resourceServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<ResourceController>(ResourceController);
--- apps/backend/src/resource/services/resource.service.spec.ts
+++ apps/backend/src/resource/services/resource.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResourceService } from './resource.service';
+import { ResourceRepositoryImpl } from '../repositories/resource.repository.impl';
+import { InMemoryResourceRepository } from '../../../test/__mocks__/repositories/in-memory/resource.repository';
+import { resourcesMock } from '../../../test/__mocks__/data/resources';
 
 describe('ResourceService', () => {
   let service: ResourceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ResourceService],
+      providers: [
+        ResourceService,
+        {
+          provide: ResourceRepositoryImpl,
+          useClass: InMemoryResourceRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ResourceService>(ResourceService);
@@ -14,5 +23,36 @@ describe('ResourceService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getResources', () => {
+    it('should return resources like name ', async () => {
+      const query = {
+        name: 'User',
+      };
+      const result = await service.getResources(query);
+      expect(result).toEqual(resourcesMock.expectedResource);
+    });
+
+    it('should return all resources', async () => {
+      const result = await service.getResources({});
+      expect(result).toEqual(resourcesMock.resources);
+    });
+  });
+
+  describe('getById', () => {
+    it('should return the resource by id', async () => {
+      const id = '967b7825-68d7-4643-a2fa-de98aea6c066';
+      const result = await service.getById(id);
+      expect(result).toEqual(resourcesMock.resources[1]);
+    });
+  });
+
+  describe('getByName', () => {
+    it('should return the resource by name', async () => {
+      const name = 'Product registration';
+      const result = await service.getByName(name);
+      expect(result).toEqual([resourcesMock.resources[1]]);
+    });
   });
 });
